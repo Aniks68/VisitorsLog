10b2006ef5d0d58bf89d8378ed1c788eca123fb3
Aniks68 <ikechukwu.anene@decagon.dev>
2022-03-27 23:53:33 +0100

    created addStaff method

--- src/main/java/com/example/staffvisitorproject/repository/StaffRepository.java
+++ src/main/java/com/example/staffvisitorproject/repository/StaffRepository.java
@@ -2,6 +2,12 @@ package com.example.staffvisitorproject.repository;
 
 import com.example.staffvisitorproject.model.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface StaffRepository extends JpaRepository<Staff, Long> {
+    Optional<Staff> findByEmailAddress(String email);
+    Optional<Staff> findByEmailAddressAndPassword(String email, String password);
 }
--- src/main/java/com/example/staffvisitorproject/service/serviceImplementation/StaffServiceImpl.java
+++ src/main/java/com/example/staffvisitorproject/service/serviceImplementation/StaffServiceImpl.java
@@ -2,11 +2,25 @@ package com.example.staffvisitorproject.service.serviceImplementation;
 
 import com.example.staffvisitorproject.dto.StaffRegDTO;
 import com.example.staffvisitorproject.model.Staff;
+import com.example.staffvisitorproject.repository.StaffRepository;
 import com.example.staffvisitorproject.service.StaffService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.modelmapper.ModelMapper;
 
 import java.util.List;
 
+@Service
 public class StaffServiceImpl implements StaffService {
+    private final StaffRepository staffRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public StaffServiceImpl(StaffRepository staffRepository, ModelMapper modelMapper) {
+        this.staffRepository = staffRepository;
+        this.modelMapper = modelMapper;
+    }
+
     @Override
     public String loginUser(String username, String password) {
         return null;
@@ -24,6 +38,12 @@ public class StaffServiceImpl implements StaffService {
 
     @Override
     public Staff addStaff(StaffRegDTO staffDetails) {
+        final Staff staff = staffRepository.findByEmailAddress(staffDetails.getEmail()).orElse(null);
+
+        if (staff.equals(null)) {
+            final Staff newStaff = staffRepository.save(modelMapper.map(staffDetails, Staff.class));
+            return newStaff;
+        }
         return null;
     }
 }

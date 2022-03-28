1db84e55762e640703d9dcda501e915b477d7d83
Aniks68 <ikechukwu.anene@decagon.dev>
2022-03-28 14:11:36 +0100

    added swagger configuration and dependencies

--- src/main/java/com/example/staffvisitorproject/model/Visitor.java
+++ src/main/java/com/example/staffvisitorproject/model/Visitor.java
@@ -12,11 +12,15 @@ public class Visitor {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "visitor_name", nullable = false)
     private String fullName;
 
+    @Column(name = "phone_number", nullable = false)
     private String phoneNumber;
 
+    @Column(name = "email_address", nullable = false)
     private String emailAddress;
 
+    @Column(name = "address", nullable = false)
     private String homeAddress;
 }
